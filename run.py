3b3bc8822c651482f89ef955e3e41dbf3b7af7b7
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-02-23 15:31:58 +0000

    finish calculations for surplus data

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-def calculate_surplus_data(sales_data):
+def calculate_surplus_data(sales_row):
     """
       
     Compare sales with stock and calculate the surplus for each item type.
@@ -69,8 +69,13 @@ def calculate_surplus_data(sales_data):
     """
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
-
+   
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 def main():
     """ 
     Runs all program functions
@@ -78,7 +83,7 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
