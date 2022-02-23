e3c48b2ca26b4ff1f3f5215f9719f2bc68ff6446
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-02-23 15:39:51 +0000

    linked surplus to google doc

--- run.py
+++ run.py
@@ -31,6 +31,7 @@ def get_sales_data():
             print("Data is valid!")
             break
     return sales_data
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -58,6 +59,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the data calculated
+    """
+    print("Updating surplus worksheet...")
+    sales_worksheet = SHEET.worksheet("surplus")
+    sales_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 def calculate_surplus_data(sales_row):
     """
       
@@ -84,6 +94,8 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
